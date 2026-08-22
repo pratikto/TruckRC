eb27f448c5745a52a372a6f1020ce29947b4f008
pratikto <12679156+pratikto@users.noreply.github.com>
2026-08-23 06:06:25 +0700

    Mirror logger output to USB and WebSerial

--- include/Logger.h
+++ include/Logger.h
@@ -15,14 +15,13 @@
   #define DEBUG_COLOR 1
 #endif
 
-// ANSI colors (can be disabled via DEBUG_COLOR=0)
 #if DEBUG_COLOR
-  #define C_RED     "\x1b[31m"
-  #define C_YEL     "\x1b[33m"
-  #define C_GRN     "\x1b[32m"
-  #define C_CYN     "\x1b[36m"
-  #define C_MAG     "\x1b[35m"
-  #define C_RST     "\x1b[0m"
+  #define C_RED     "\\x1b[31m"
+  #define C_YEL     "\\x1b[33m"
+  #define C_GRN     "\\x1b[32m"
+  #define C_CYN     "\\x1b[36m"
+  #define C_MAG     "\\x1b[35m"
+  #define C_RST     "\\x1b[0m"
 #else
   #define C_RED     ""
   #define C_YEL     ""
@@ -32,24 +31,21 @@
   #define C_RST     ""
 #endif
 
-// Levels
 #define LVL_ERROR   0
 #define LVL_WARN    1
 #define LVL_INFO    2
 #define LVL_DEBUG   3
 #define LVL_VERBOSE 4
 
-// Timestamp helper (ms since boot)
 static inline const char* __ts(char* buf, size_t n) {
   snprintf(buf, n, "%lu", (unsigned long)millis());
   return buf;
 }
 
-// ===== Multi-sink support (Serial + Bluetooth or others) =====
-extern Print* LOG_OUT1;   // define in main.cpp, e.g. &Serial
-// extern Print* LOG_OUT2;   // optional second sink, e.g. &BluetoothSerial
+// Implemented by main.cpp. It mirrors every log line to USB and,
+// when available, to WebSerial.
+void writeLogLine(const char* line);
 
-// Core print enable
 #ifdef DEBUG
   #define __LOG_ENABLED 1
 #else
@@ -57,22 +53,18 @@ extern Print* LOG_OUT1;   // define in main.cpp, e.g. &Serial
 #endif
 
 #if __LOG_ENABLED
-  // Build one line and write to both sinks if set
-  #define __LOG_LINE(color, lvlstr, tag, fmt, ...) do {                               \
-    char __tbuf[16];                                                                  \
-    char __buf[256];                                                                  \
-    int __n = snprintf(__buf, sizeof(__buf), "%s[%s][%s][%s] " fmt "%s\r\n",          \
-                       color, __ts(__tbuf, sizeof(__tbuf)), lvlstr, tag,              \
-                       ##__VA_ARGS__, C_RST);                                         \
-    if (__n < 0) break;                                                               \
-    if (LOG_OUT1) LOG_OUT1->write((const uint8_t*)__buf, (size_t)min(__n, (int)sizeof(__buf)-1)); \
-    /* if (LOG_OUT2) LOG_OUT2->write((const uint8_t*)__buf, (size_t)min(__n, (int)sizeof(__buf)-1));*/ \
+  #define __LOG_LINE(color, lvlstr, tag, fmt, ...) do {                         \
+    char __tbuf[16];                                                            \
+    char __buf[256];                                                            \
+    int __n = snprintf(__buf, sizeof(__buf), "%s[%s][%s][%s] " fmt "%s\\r\\n", \
+                       color, __ts(__tbuf, sizeof(__tbuf)), lvlstr, tag,        \
+                       ##__VA_ARGS__, C_RST);                                   \
+    if (__n >= 0) writeLogLine(__buf);                                          \
   } while(0)
 #else
   #define __LOG_LINE(color, lvlstr, tag, fmt, ...) do {} while(0)
 #endif
 
-// Public macros per level (compile-time filtered by DEBUG_LEVEL)
 #if __LOG_ENABLED && (DEBUG_LEVEL >= LVL_ERROR)
   #define LOGE(tag, fmt, ...) __LOG_LINE(C_RED,  "ERROR", tag, fmt, ##__VA_ARGS__)
 #else
