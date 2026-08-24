ec63e33023c3dda478057b65bc0751e0654c143c
Pratikto <pratikto.sulthoni.h@mail.ugm.ac.id>
2026-08-24 21:59:40 +0700

    add wifi and password

--- include/secrets.example.h
+++ include/secrets.example.h
@@ -2,5 +2,5 @@
 
 // Copy this file to include/secrets.h, then enter your own Wi-Fi credentials.
 // include/secrets.h is ignored by Git and must never be committed.
-#define WIFI_SSID "Nama-WiFi-IndiHome"
-#define WIFI_PASSWORD "Password-WiFi"
+#define WIFI_SSID "SSID name"
+#define WIFI_PASSWORD "password"
